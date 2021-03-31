bb69631c9b97300f07270cbf0f8c2bd046fd9ead
Voidfractur <73500723+Voidfractur@users.noreply.github.com>
2021-03-31 15:01:42 -0600

    add prueba

---
+++ examenphp/src/main/java/modelo/Prueba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author spart
+ */
+public class Prueba {
+	
+        public void getsuma(String text){
+            System.out.println(text.concat(" Se imprimió la suma echa en PHP desde Java"));
+        }
+        
+        public static void main(String[] args) {
+        System.out.println((" Se imprimió la suma echa en PHP desde Java"));
+    }
+}
